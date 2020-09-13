bc6d72bc50926a405d39bfd4a10eff3bd5a719a1
fajjet <fajjet@yandex.ru>
2020-09-13 15:42:04 +0300

    fix: return paths back

--- containers/index.tsx
+++ containers/index.tsx
@@ -1,19 +1,19 @@
-export { default as Page } from "containers/core/page/page";
-export { default as Header } from "containers/core/header/header";
-export { default as Footer } from "containers/core/footer/footer";
+export { default as Page } from "./core/page/page";
+export { default as Header } from "./core/header/header";
+export { default as Footer } from "./core/footer/footer";
 
-export { default as Home } from "containers/pages/home/home";
-export { default as SignUp } from "containers/pages/sign-up/sign-up";
-export { default as SignIn } from "containers/pages/sign-in/sign-in";
-export { default as Profile } from "containers/pages/profile/profile";
-export { default as Users } from "containers/pages/users/users";
-export { default as User } from "containers/pages/user/user";
-export { default as Post } from "containers/pages/post/post";
-export { default as Error } from "containers/pages/error/error";
+export { default as Home } from "./pages/home/home";
+export { default as SignUp } from "./pages/sign-up/sign-up";
+export { default as SignIn } from "./pages/sign-in/sign-in";
+export { default as Profile } from "./pages/profile/profile";
+export { default as Users } from "./pages/users/users";
+export { default as User } from "./pages/user/user";
+export { default as Post } from "./pages/post/post";
+export { default as Error } from "./pages/error/error";
 
-export { default as SignUpForm } from "containers/forms/sign-up-form/sign-up-form";
-export { default as SignInForm } from "containers/forms/sign-in-form/sign-in-form";
-export { default as SubmitPost } from "containers/forms/submit-post/submit-post";
-export { default as AddComment } from "containers/forms/add-comment/add-comment";
+export { default as SignUpForm } from "./forms/sign-up-form/sign-up-form";
+export { default as SignInForm } from "./forms/sign-in-form/sign-in-form";
+export { default as SubmitPost } from "./forms/submit-post/submit-post";
+export { default as AddComment } from "./forms/add-comment/add-comment";
 
-export { default as Posts } from "containers/posts/posts";
+export { default as Posts } from "./posts/posts";
