b5cf5464965b854d2aa828df97f7cba3f4fa5ceb
fajjet <fajjet@yandex.ru>
2020-09-13 18:08:27 +0300

    fix: return back old path names, now linux should work well

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 import Cookies from "js-cookie";
 
-import { Profile } from 'containers';
+import { Profile } from "containers";
 import { Helmet } from "components";
-import { CustomNextPageContext } from 'types';
+import { CustomNextPageContext } from "types";
 
 interface Props {
   pageProps: {
     tokenId: string;
-  }
+  };
 }
 
 const ProfilePage = (props: Props) => {
   const { tokenId } = props.pageProps || {};
   return (
     <>
-      <Helmet title={'Profile'}/>
-      <Profile tokenId={tokenId}/>
+      <Helmet title={"Profile"} />
+      <Profile tokenId={tokenId} />
     </>
-  )
+  );
 };
 
 ProfilePage.getInitialProps = async (context: CustomNextPageContext) => {
-  let tokenId = '';
+  let tokenId = "";
   if (context.req) {
     tokenId = context.req?.cookies?.tokenId;
   } else {
-    tokenId = Cookies.get('tokenId') || '';
+    tokenId = Cookies.get("tokenId") || "";
   }
   return {
     tokenId,
-  }
+  };
 };
 
 export default ProfilePage;
